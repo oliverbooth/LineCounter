a2c52b70de3ac3906452f6c4f9940072b551b013
Oliver Booth <me@olivr.me>
2022-12-24 14:05:30 +0000

    Use top-level statements

--- LineCounter/Program.cs
+++ LineCounter/Program.cs
@@ -1,6 +1,4 @@
-﻿namespace LineCounter;
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -8,97 +6,92 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Colorful;
 using CommandLine;
+using LineCounter;
 using Console = Colorful.Console;
 
-internal static class Program
+await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(CountLinesAsync).ConfigureAwait(true);
+
+static async Task CountLinesAsync(Options options)
 {
-    private static async Task Main(string[] args)
+    if (options.Verbose)
     {
-        await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(CountLinesAsync).ConfigureAwait(true);
+        Console.WriteLine("Ignoring directories:");
+        foreach (var ignore in options.Ignore.Select(Path.GetFullPath))
+        {
+            Console.WriteLine($"- {ignore}");
+        }
     }
 
-    private static async Task CountLinesAsync(Options options)
+    var regex = new Regex(options.Pattern, RegexOptions.Compiled);
+    var path = Path.GetFullPath(options.Path);
+    var searchOption = options.Recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+    var files = Directory.GetFiles(path, "*", searchOption);
+    var count = 0;
+
+    foreach (var file in files)
     {
-        if (options.Verbose)
+        var directory = Path.GetDirectoryName(file);
+        if (directory is null)
         {
-            Console.WriteLine("Ignoring directories:");
-            foreach (var ignore in options.Ignore.Select(Path.GetFullPath))
-            {
-                Console.WriteLine($"- {ignore}");
-            }
-        }
-
-        var regex = new Regex(options.Pattern, RegexOptions.Compiled);
-        var path = Path.GetFullPath(options.Path);
-        var searchOption = options.Recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
-        var files = Directory.GetFiles(path, "*", searchOption);
-        var count = 0;
-
-        foreach (var file in files)
-        {
-            var directory = Path.GetDirectoryName(file);
-            if (directory is null)
-            {
-                if (options.Verbose)
-                {
-                    var formatter = new[] { new Formatter("NULL", Color.Red), new Formatter(file, Color.LightGray) };
-                    Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
-                }
-
-                continue;
-            }
-
-            if (options.Ignore.Select(Path.GetFullPath).Any(i => directory.StartsWith(i)))
-            {
-                if (options.Verbose)
-                {
-                    var formatter = new[] { new Formatter("IGNORE", Color.Orange), new Formatter(file, Color.LightGray) };
-                    Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
-                }
-
-                continue;
-            }
-
-            if (!regex.IsMatch(file))
-            {
-                if (options.Verbose)
-                {
-                    var formatter = new[] { new Formatter("NO_MATCH", Color.Yellow), new Formatter(file, Color.LightGray) };
-                    Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
-                }
-
-                continue;
-            }
-
-            var lines = (await File.ReadAllLinesAsync(file).ConfigureAwait(false)) as IEnumerable<string>;
-
-            if (!options.Whitespace)    
-            {
-                lines = lines.Where(line => !string.IsNullOrWhiteSpace(line));
-            }
-
-            if (!string.IsNullOrWhiteSpace(options.IgnoreChars))
-            {
-                lines = lines.Where(line => line.Trim().Length > 0 && options.IgnoreChars.IndexOf(line.Trim()[0]) != 0);
-            }
-
-            var fileCount = lines.Count();
             if (options.Verbose)
             {
-                var formatter = new[] { new Formatter(fileCount, Color.Cyan), new Formatter(file, Color.LightGray) };
+                var formatter = new[] {new Formatter("NULL", Color.Red), new Formatter(file, Color.LightGray)};
                 Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
             }
 
-            count += fileCount;
+            continue;
         }
 
-        Console.ResetColor();
+        if (options.Ignore.Select(Path.GetFullPath).Any(i => directory.StartsWith(i)))
+        {
+            if (options.Verbose)
+            {
+                var formatter = new[] {new Formatter("IGNORE", Color.Orange), new Formatter(file, Color.LightGray)};
+                Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
+            }
 
+            continue;
+        }
+
+        if (!regex.IsMatch(file))
+        {
+            if (options.Verbose)
+            {
+                var formatter = new[] {new Formatter("NO_MATCH", Color.Yellow), new Formatter(file, Color.LightGray)};
+                Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
+            }
+
+            continue;
+        }
+
+        var lines = (await File.ReadAllLinesAsync(file).ConfigureAwait(false)) as IEnumerable<string>;
+
+        if (!options.Whitespace)
+        {
+            lines = lines.Where(line => !string.IsNullOrWhiteSpace(line));
+        }
+
+        if (!string.IsNullOrWhiteSpace(options.IgnoreChars))
+        {
+            lines = lines.Where(line => line.Trim().Length > 0 && options.IgnoreChars.IndexOf(line.Trim()[0]) != 0);
+        }
+
+        var fileCount = lines.Count();
         if (options.Verbose)
         {
-            Console.Write("Total line count: ");
+            var formatter = new[] {new Formatter(fileCount, Color.Cyan), new Formatter(file, Color.LightGray)};
+            Console.WriteLineFormatted("[{0}] {1}", Color.Gray, formatter);
         }
 
-        Console.WriteLine(count);
+        count += fileCount;
     }
+
+    Console.ResetColor();
+
+    if (options.Verbose)
+    {
+        Console.Write("Total line count: ");
+    }
+
+    Console.WriteLine(count);
 }
